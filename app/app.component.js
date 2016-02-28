d85ba282a0923b0b048c3f19169d33f2c427f6b8
Tri Nguyen <nguyen190887@gmail.com>
2016-02-28 22:23:31 +0700

    Rename app; Handle selected; Include email list generation button

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent, REVIEWLIST;
+    var AppComponent, hasSelectedTeam, REVIEWLIST;
     return {
         setters:[
             function (core_1_1) {
@@ -21,30 +21,57 @@ System.register(['angular2/core'], function(exports_1) {
                     this.teamFilter = '';
                     this.reviewTeams = REVIEWLIST;
                 }
+                AppComponent.prototype.onSelect = function (team) {
+                    team.selected = !team.selected;
+                    this.mailList = '';
+                    this.hasSelected = hasSelectedTeam(this.reviewTeams);
+                };
+                AppComponent.prototype.generateMailList = function () {
+                    this.mailList = '';
+                    for (var _i = 0, _a = this.reviewTeams; _i < _a.length; _i++) {
+                        var team = _a[_i];
+                        if (team.selected) {
+                            for (var _b = 0, _c = team.reviewers; _b < _c.length; _b++) {
+                                var reviewer = _c[_b];
+                                this.mailList += reviewer.email + '; ';
+                            }
+                        }
+                    }
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'review-list',
-                        template: "\n    <h1>Review Teams</h1>\n    \n    <div>\n      <label>Type your filter: </label>\n      <input [(ngModel)]=\"teamFilter\" />\n    </div>\n\n    <div *ngFor=\"#team of reviewTeams\">\n      <div *ngIf=\"team.name.toLowerCase().indexOf(teamFilter.toLowerCase()) >= 0\">\n        <h3>{{team.id}} - {{team.name}}</h3>\n        <ul>\n          <li *ngFor=\"#reviewer of team.reviewers\">{{reviewer.name}}</li>\n        </ul>\n        </div>\n    </div>\n    "
+                        template: "\n    <h1>Review Teams</h1>\n    \n    <div>\n      <label>Type your filter: </label>\n      <input [(ngModel)]=\"teamFilter\" />\n    </div>\n\n    <div *ngFor=\"#team of reviewTeams\"\n      [class.selected]=\"team.selected\"\n      (click)=\"onSelect(team)\">\n      <div *ngIf=\"team.name.toLowerCase().indexOf(teamFilter.toLowerCase()) >= 0\">\n        <h3>{{team.id}} - {{team.name}}</h3>\n        <ul>\n          <li *ngFor=\"#reviewer of team.reviewers\">{{reviewer.name}}</li>\n        </ul>\n        </div>\n    </div>\n\n    <div *ngIf=\"hasSelected\">\n      <div>\n        <!-- You selected: {{selectedTeam.name}} -->\n        <br/>\n        <span *ngIf=\"mailList\">Mail list: {{mailList}}</span>\n      </div>\n      <div>\n        <button (click)=\"generateMailList()\">Generate mail list</button>\n      </div>\n    </div>\n    ",
+                        styles: ["\n    .selected {\n      background-color: #ddcc00;\n      color: white;\n    }\n  "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            hasSelectedTeam = function (teams) {
+                for (var i = 0; i < teams.length; i++) {
+                    if (teams[i].selected) {
+                        return true;
+                    }
+                }
+                return false;
+            };
             REVIEWLIST = [
                 {
                     id: 1,
-                    name: 'Kamikaze',
+                    name: 'Super women',
                     reviewers: [
-                        { id: 101, name: 'Tina' },
-                        { id: 102, name: 'Kym' }
-                    ] },
+                        { id: 101, name: 'Tina', email: 'tina@mail.com' },
+                        { id: 102, name: 'Kym', email: 'kym@mail.com' }
+                    ]
+                },
                 {
                     id: 2,
-                    name: 'Cosmo',
+                    name: 'Advertiser',
                     reviewers: [
-                        { id: 101, name: 'Valentyna' },
-                        { id: 102, name: 'Josh' }
+                        { id: 101, name: 'Valentyna', email: 'val@mail.com' },
+                        { id: 102, name: 'Josh', email: 'josh@mail.com' }
                     ]
                 }
             ];
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,13 @@ interface ReviewTeam {
   id: number;
   name: string;
   reviewers: Reviewer[];
+  selected: boolean;
 }
 
 interface Reviewer {
   id: number;
   name: string;
+  email: string;
 }
 
 @Component({
@@ -21,7 +23,9 @@ interface Reviewer {
       <input [(ngModel)]="teamFilter" />
     </div>
 
-    <div *ngFor="#team of reviewTeams">
+    <div *ngFor="#team of reviewTeams"
+      [class.selected]="team.selected"
+      (click)="onSelect(team)">
       <div *ngIf="team.name.toLowerCase().indexOf(teamFilter.toLowerCase()) >= 0">
         <h3>{{team.id}} - {{team.name}}</h3>
         <ul>
@@ -29,27 +33,73 @@ interface Reviewer {
         </ul>
         </div>
     </div>
-    `
+
+    <div *ngIf="hasSelected">
+      <div>
+        <!-- You selected: {{selectedTeam.name}} -->
+        <br/>
+        <span *ngIf="mailList">Mail list: {{mailList}}</span>
+      </div>
+      <div>
+        <button (click)="generateMailList()">Generate mail list</button>
+      </div>
+    </div>
+    `,
+  styles: [`
+    .selected {
+      background-color: #ddcc00;
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
   teamFilter = '';
   reviewTeams = REVIEWLIST;
+  hasSelected: boolean;
+  mailList: string;
+
+  onSelect(team: ReviewTeam) {
+    team.selected = !team.selected;
+    this.mailList = '';
+    this.hasSelected = hasSelectedTeam(this.reviewTeams);
+  }
+
+  generateMailList() {
+    this.mailList = '';
+    for (var team of this.reviewTeams) {
+      if (team.selected) {
+        for (var reviewer of team.reviewers) {
+          this.mailList += reviewer.email + '; ';
+        }
+      }
+    }
+  }
+}
+
+var hasSelectedTeam = function(teams: Array) {
+  for (var i = 0; i < teams.length; i++) {
+    if (teams[i].selected) {
+      return true;
+    }
+  }
+  return false;
 }
 
 var REVIEWLIST: ReviewTeam[] = [
   { 
     id: 1, 
-    name: 'Kamikaze', 
+    name: 'Super women', 
     reviewers: [
-      {id: 101, name: 'Tina'}, 
-      {id:102, name: 'Kym'}
-    ] },
+      {id: 101, name: 'Tina', email: 'tina@mail.com'}, 
+      {id:102, name: 'Kym', email: 'kym@mail.com'}
+    ]
+  },
   { 
     id: 2,
-    name: 'Cosmo',
+    name: 'Advertiser',
     reviewers: [
-      { id: 101, name: 'Valentyna' },
-      { id: 102, name: 'Josh' }
+      { id: 101, name: 'Valentyna', email: 'val@mail.com' },
+      { id: 102, name: 'Josh', email: 'josh@mail.com'}
     ]
   }
 ];
